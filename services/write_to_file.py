e1f3f7c091e51605803f6ca7760ac3fa4843dd06
Sunny Kasera <sunnykasera3107@gmail.com>
2026-04-07 22:46:15 +0530

    Fixing file writer tool

--- services/write_to_file.py
+++ services/write_to_file.py
@@ -9,15 +9,20 @@ class WriteToFile(BaseTool):
     def _run(self, query: str) -> str:
         print("✅ NEW WRITER RUNNING")
         filename = os.path.join("data/generated_posts", f"{datetime.now().strftime('%Y%m%d_%H%M%S')}-linkedin-post.txt")
-        query = query.replace('\u2011', "-")
         try:
             with open(filename, "w") as file:
-                file.write(query)
+                file.write(self.clean_text(query))
         except Exception as e:
             print(f"Error: {str(e)}")
 
         return f"Content successfully written to {query}"
     
+    def clean_text(self, text: str) -> str:
+        return (
+            text.replace("\u2011", "-")
+                .replace("\u2013", "-")
+                .replace("\u2014", "-")
+        )
 
 if __name__ == "__main__":
     writefile = WriteToFile()
